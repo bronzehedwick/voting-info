4b7fa13fb85b17798eb7166f91412ced0e2b2355
Chris DeLuca <code@chrisdeluca.me>
2020-10-10 16:34:37 -0400

    Remove GridBox
    This is no longer needed in the current design, so remove from the
    output for now.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 
 import Ctas from "../components/ctas"
 import DatesDeadlines from "../components/dates-deadlines"
-import GridBox from "../components/grid-box"
 import Layout from "../components/layout"
 import Mapbox from "../components/Mapbox"
 import Polling from "../components/polling"
@@ -18,18 +17,16 @@ const IndexPage = () => {
         items={strings.datesDeadlines.items}
         headline={strings.datesDeadlines.headline}
       />
-      <GridBox>
-        <Polling
-          items={strings.polling.items}
-          headline={strings.polling.headline}
-          submit={strings.polling.submit}
-          setVoterData={setVoterData}
-        />
-        <Mapbox voterData={voterData} />
-        <Ctas items={strings.ctas} />
-      </GridBox>
-    </Layout>
-  )
+      <Polling
+        items={strings.polling.items}
+        headline={strings.polling.headline}
+        submit={strings.polling.submit}
+        setVoterData={setVoterData}
+      />
+      <Mapbox voterData={voterData} />
+      <Ctas items={strings.ctas} />
+  </Layout>
+)
 }
 
 export default IndexPage

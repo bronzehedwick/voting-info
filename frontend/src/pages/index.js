fbc1a8ed2807e7f3fa98e6d6eae744ebb9052f95
Chris DeLuca <code@chrisdeluca.me>
2020-10-23 19:14:14 -0400

    Add voting schedule box
    Use same style as tips info box, and align the box center.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -31,6 +31,23 @@ const IndexPage = () => {
       />
       <Mapbox voterData={voterData} />
       <Ctas items={strings.ctas} />
+      <br />
+      <div className="two-up">
+        <section className="tips">
+          <h2 className="tips__headline">Early Voting Schedule</h2>
+          <ul className="tips__list">
+            <li className="tips__item">Saturday October 24, 2020 10 AM to 4 PM</li>
+            <li className="tips__item">Sunday October 25, 2020 10 AM to 4 PM</li>
+            <li className="tips__item">Monday October 26, 2020 7 AM to 3 PM</li>
+            <li className="tips__item">Tuesday October 27, 2020 12 PM to 8 PM</li>
+            <li className="tips__item">Wednesday October 28, 2020 12 PM to 8 PM</li>
+          </ul>
+          <h2 className="tips__headline">Normal Voting Schedule</h2>
+          <ul className="tips__list">
+            <li className="tips__item">Tuesday November 3, 2020 6 AM to 9 PM</li>
+          </ul>
+        </section>
+      </div>
     </Layout>
   )
 }
